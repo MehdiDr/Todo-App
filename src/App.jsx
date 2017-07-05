7db66033c9c8408351dd103533c13c0cbc5df3b6
MehdiDr <mehdidruon@gmail.com>
2017-07-05 23:37:26 +0200

    Add container TodoComponents

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
-import TodoList from './components/TodoList';
+import TodoComponents from './containers/TodoComponents';
 
 class App extends Component {
   render() {
     return (
-      <TodoList />
+      <div>
+        <div className="nav">
+          <h1 className="title nav-center">Todo List</h1>
+      </div>
+      <TodoComponents />
+      </div>
     );
   }
 }
