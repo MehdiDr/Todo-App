48d467c354be83f2be8013a3aadb75753bb29e57
Mehdi Druon <mehdidruon@gmail.com>
2017-07-08 18:58:31 +0200

    Revert "Add react-router and history for github pages"
    This reverts commit 006ab24bd49567a97097ef350f82241edb7c9dbe.
    
    Conflicts:
    	src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
 import React, { Component } from 'react';
 import TodoComponents from './containers/TodoComponents';
-import createBrowserHistory from 'history/createBrowserHistory';
-
-import { BrowserRouter as Router, HashRouter } from 'react-router-dom';
-
-const history = createBrowserHistory();
 
 class App extends Component {
   render() {
     return (
-      <HashRouter history={history}>
-        <div>
-          <div className="nav">
-            <h1 className="title nav-center">Todo List</h1>
-          </div>
-          <TodoComponents />
+      <div>
+        <div className="nav">
+          <h1 className="title nav-center">Todo List</h1>
         </div>
-      </HashRouter>
+        <TodoComponents />
+      </div>
     );
   }
 }
