6fe49c4b512aab84a180c5bdbc2c1359a820bbea
Mehdi Druon <mehdidruon@gmail.com>
2017-07-10 00:34:09 +0200

    Add some css (yeah i'm a little bit perfectionist)

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,33 +3,31 @@ import PropTypes from 'prop-types';
 import TodoItem from './TodoItem';
 
 const TodoList = props => (
-  <div className="section">
-    <ul>
-      {props.todos.map(item => (
-        <div className="box content">
-          <TodoItem
-            key={item.id}
-            id={item.id}
-            name={item.name}
-            description={item.description}
-            deadline={item.deadline}
-            finished={item.finished}
-            archived={item.archived}
-            onTodoClick={() => props.onTodoClick(item.id)}
-          />
-          <hr />
-          <div className="field is-grouped">
-            <p className="control">
-              <a className="button is-warning" onClick={() => props.onArchiveClick(item.id)}>{item.archived ? 'Unarchive' : 'Archive'}</a>
-            </p>
-            <p className="control">
-              <a className="button is-danger" onClick={() => props.onDeleteClick(item.id)}>Delete</a>
-            </p>
-          </div>
+  <ul>
+    {props.todos.map(item => (
+      <div className="box content">
+        <TodoItem
+          key={item.id}
+          id={item.id}
+          name={item.name}
+          description={item.description}
+          deadline={item.deadline}
+          finished={item.finished}
+          archived={item.archived}
+          onTodoClick={() => props.onTodoClick(item.id)}
+        />
+        <hr />
+        <div className="field is-grouped">
+          <p className="control">
+            <a className="button is-warning" onClick={() => props.onArchiveClick(item.id)}>{item.archived ? 'Unarchive' : 'Archive'}</a>
+          </p>
+          <p className="control">
+            <a className="button is-danger" onClick={() => props.onDeleteClick(item.id)}>Delete</a>
+          </p>
         </div>
-      ))}
-    </ul>
-  </div>
+      </div>
+    ))}
+  </ul>
 );
 
 TodoList.propTypes = {
