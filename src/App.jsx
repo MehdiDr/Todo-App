fd452cb0b2706bb08f82aa8f83c0504db5d3f695
Mehdi Druon <mehdidruon@gmail.com>
2017-07-09 20:58:40 +0200

    Fix an error with travis

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import AddTodo from './containers/AddTodo';
 import TodoFilterContainer from './containers/TodoFilterContainer';
 import TodoListContainer from './containers/TodoListContainer';
+import reducers from './reducers/index';
+
+const store = createStore(reducers);
 
 const App = () => (
-  <div>
-    <div className="nav">
-      <h1 className="title nav-center">Todo List</h1>
+  <Provider store={store}>
+    <div>
+      <div className="nav">
+        <h1 className="title nav-center">Todo List</h1>
+      </div>
+      <AddTodo />
+      <hr />
+      <TodoFilterContainer />
+      <hr />
+      <TodoListContainer />
     </div>
-    <TodoFilterContainer />
-    <hr />
-    <TodoListContainer />
-    <hr />
-    <AddTodo />
-  </div>
+  </Provider>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from './reducers';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-const store = createStore(reducers);
-
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root'),
+  <App />, document.getElementById('root'),
 );
 
 registerServiceWorker();
